1849c287bb2e1866d14e674ad1a3b2d81248c5ca
aman <aman.dj75@gmail.com>
2020-09-10 19:30:07 +0530

    Updates

---
+++ precache-manifest.4ebfa80be9136b660c445199ece3616d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1527042ddaa005d6b8b9275d8306f2c",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "d85f62f8aed3c671fc8a",
+    "url": "/MonsterRolodex/static/css/main.010df242.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/MonsterRolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85f62f8aed3c671fc8a",
+    "url": "/MonsterRolodex/static/js/main.2da82958.chunk.js"
+  },
+  {
+    "revision": "dec060da763e5a21e9e8",
+    "url": "/MonsterRolodex/static/js/runtime-main.48dbe2d5.js"
+  }
+]);
